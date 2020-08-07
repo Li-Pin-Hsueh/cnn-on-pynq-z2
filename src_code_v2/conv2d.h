0696470b1cdb97e25bc7b8ecf142e98df54d6c27
Vincent <pinxue.career@gmail.com>
2020-08-07 19:59:22 +0800

    new version of code

---
+++ src_code_v2/conv2d.h
@@ -0,0 +1,11 @@
+#include "hls_stream.h"
+#define FILTER_SIZE 3	// 3*3 
+#define CHANNEL 3	// a Filter includes 3 Kernels
+
+struct AXI_T {
+	float data;
+	bool last;
+};
+typedef hls::stream<AXI_T> STREAM_T;
+// filter size K_SIZE*K_SZIE*
+void conv2d(STREAM_T &filter, STREAM_T &input, STREAM_T &output, int input_size, int batch, int n );
