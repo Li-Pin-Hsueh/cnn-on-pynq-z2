10505a8a20f907a42ba3e29647b4d6baf8fffaf1
Vincent <pinxue.career@gmail.com>
2020-08-08 13:26:28 +0800

    Completed data storing

--- src_code_v2/tb_conv2d.cpp
+++ src_code_v2/tb_conv2d.cpp
@@ -117,14 +117,55 @@ int main(void){
 	STREAM_T stream_filter, stream_input, stream_output ;
 
 // store 3*3*3*n filter data in stream<>
-	
+	int count = 0, channel = 0, row = 0, col = 0;
+	for( count=0; count < filter_number; count++ ){
+		for( channel=0; channel < filter_channel; channel++ ){
+			for( row=0; row < filter_size; row++ ){
+				for( col=0; col < filter_size; col++ ){
+
+					// check the last data
+					if( count==filter_number && channel==filter_channel && row==filter_size && col==filter_size)
+						break;
+
+					// not the last data
+					axi_tmp.data = filter[count][channel][row][col];
+					axi_tmp.last = 0;
+					stream_filter << axi_tmp;
+				}
+			}
+		}
+	}
+	// store the last data into the stream<>
+	axi_tmp.data = filter[filter_number-1][filter_channel-1][filter_size-1][filter_size-1];
+	axi_tmp.last = 1;
+	stream_filter << axi_tmp;
 
 // store input volume data in stream<>
-	
 
+	for( count=0; count < input_number; count++ ){
+		for( channel=0; channel < input_channel; channel++ ){
+			for( row=0; row < input_size; row++ ){
+				for( col=0; col < input_size; col++ ){
+
+					// check the last data
+					if( count==input_number && channel==input_channel && row==input_size && col==input_size)
+						break;
+
+					// not the last data
+					axi_tmp.data = input[count][channel][row][col];
+					axi_tmp.last = 0;
+					stream_input << axi_tmp;
+				}
+			}
+		}
+	}
+	// store the last data into the stream<>
+	axi_tmp.data = input[input_number-1][input_channel-1][input_size-1][input_size-1];
+	axi_tmp.last = 1;
+	stream_input << axi_tmp;
 
 // execute the conv2d
-
+	conv2d(stream_filter, stream_input, stream_output, input_size, 1, 1);
 
 // print the answer
 	printf("====Output Volume====\n");
